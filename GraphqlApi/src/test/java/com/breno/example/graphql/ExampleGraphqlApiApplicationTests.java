2045dd808d6abf5410764e8921292039c7529df1
Breno Fachini <dev.brenofachini@gmail.com>
2021-06-08 20:53:05 -0300

    creating graphql project

---
+++ GraphqlApi/src/test/java/com/breno/example/graphql/ExampleGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.example.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGraphqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
